994d94207605c04feb3ca4c72d0a5eec2d08e763
lecto17 <khwoo_rpsit@naver.com>
2025-02-14 17:18:47 +0900

    feat: 인스타그램 보더 컴포너트 추가

---
+++ src/components/border/InstagramBorder.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+interface InstagramBorderProps {
+  children: React.ReactNode;
+  padding?: string;
+  rounded?:
+    | "rounded-xs"
+    | "rounded-sm"
+    | "rounded-md"
+    | "rounded-lg"
+    | "rounded-xl"
+    | "rounded-2xl"
+    | "rounded-full";
+  className?: string;
+}
+
+const InstagramBorder = ({
+  children,
+  padding = "p-1",
+  rounded = "rounded-full",
+  className,
+}: InstagramBorderProps) => {
+  return (
+    <div
+      className={`flex overflow-hidden bg-gradient-to-r from-[#F58529] via-[#DD2A7B] to-[#C42D91] ${padding} ${rounded} ${className}`}
+    >
+      <div
+        className={`flex-grow bg-white ${padding} ${rounded} hover:opacity-90 transition`}
+      >
+        {React.isValidElement(children)
+          ? React.cloneElement(
+              children as React.ReactElement<{ className?: string }>,
+              {
+                className: `${(children as React.ReactElement<{ className?: string }>).props.className ?? ""} ${rounded} bg-white`,
+              }
+            )
+          : children}
+      </div>
+    </div>
+  );
+};
+
+export default InstagramBorder;
