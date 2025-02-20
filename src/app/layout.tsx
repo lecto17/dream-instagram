cdd6bea3f8daaed6f3bccdcd42f8823af3b2bc8e
lecto17 <khwoo_rpsit@naver.com>
2025-02-20 13:02:26 +0900

    feat: 프로젝트 전역에 로그인한 user 상태값 주입

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import GlobalNav from "@/components/navigation/GlobalNav";
+import AuthContext from "@/context/AuthContext";
 
 const openSans = Open_Sans({
   variable: "--font-geist-mono",
@@ -14,10 +15,20 @@ export const metadata: Metadata = {
     template: "Tell your Voice | %s",
   },
   description: "Listen your neighbor's story",
-  viewport: "width=device-width, initial-scale=1",
 };
 
-export default function RootLayout({
+// 현재(nextjs 15버전) viewport는 아래와 같은 방식으로 넣을 수 있는데, 기본적으로 setting이 되는 사항이여서,
+// 별도로 넣을 필요는 없다.(Good to know: The viewport meta tag is automatically set, and manual configuration is usually unnecessary as the default is sufficient. However, the information is provided for completeness.)
+// import type { Viewport } from "next";
+
+// // export const viewport: Viewport = {
+// //   width: "device-width",
+// //   initialScale: 1,
+// //   maximumScale: 1,
+// //   userScalable: false,
+// // };
+
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
@@ -25,8 +36,10 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${openSans.className} w-full mx-auto max-w-screen-xl`}>
-        <GlobalNav />
-        <main>{children}</main>
+        <AuthContext>
+          <GlobalNav />
+          <main>{children}</main>
+        </AuthContext>
       </body>
     </html>
   );
