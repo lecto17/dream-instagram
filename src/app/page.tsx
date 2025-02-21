e1a05b681694ef111d76d3d9db500dfea6b9b403
lecto17 <khwoo_rpsit@naver.com>
2025-02-21 18:39:16 +0900

    feat: 홈화면 로그인 되어 있지 않을 경우 redirect 처리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  <div className="text-red-400">Instagram</div>;
+import { auth } from "/auth";
+import StoryList from "@/components/stories/StoryList";
+import PostList from "@/components/posts/PostList";
+import AvatarLocalNav from "@/components/navigation/AvatarLocalNav";
+import { redirect } from "next/navigation";
+import { getNameByEmail } from "@/utils/utils";
+
+export default async function HomePage() {
+  const session = await auth();
+  const user = session?.user;
+
+  const posts = [];
+
+  if (!user) {
+    redirect("/auth/login");
+  }
+
+  return (
+    <section className="flex p-5">
+      <div className="flex flex-col basis-3/4">
+        <StoryList userName={getNameByEmail(user.email)} />
+        <PostList posts={posts} />
+      </div>
+      <div className="basis-1/4 sm:hidden md:block">
+        <AvatarLocalNav user={user} />
+      </div>
+    </section>
+  );
 }
