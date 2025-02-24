81334a155d4e31b81b908227fcd19bd6aaf33f38
lecto17 <khwoo_rpsit@naver.com>
2025-02-24 20:34:07 +0900

    feat: sanity에서 user get하는 함수 추가

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,5 +1,7 @@
 import { client } from "@/sanity/sanity";
+import { fetchAllData, getQuery } from "@/service/fetch";
 import { User } from "@/types/user";
+import { getNameByEmail } from "@/utils/utils";
 
 export const createUser = async ({ id, email, ...rest }: User) => {
   await client.createIfNotExists({
@@ -11,4 +13,12 @@ export const createUser = async ({ id, email, ...rest }: User) => {
     following: [],
     bookmarks: [],
   });
+};
+
+export const getUserByname = async (name: string) => {
+  return await client.fetch(getQuery("FOLLOWINGS", getNameByEmail(name)));
+};
+
+export const getAllUser = async () => {
+  return await fetchAllData("USERS");
 };
