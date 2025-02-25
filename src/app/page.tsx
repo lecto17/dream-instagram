862826141ef35674d72b50d30e965f3bf5cdc919
lecto17 <khwoo_rpsit@naver.com>
2025-02-25 21:47:40 +0900

    chore: 프로젝트 layout 전반 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,25 @@ import StoryList from "@/components/stories/StoryList";
 import PostList from "@/components/posts/PostList";
 import AvatarLocalNav from "@/components/navigation/AvatarLocalNav";
 import { redirect } from "next/navigation";
-import { getNameByEmail } from "@/utils/utils";
+import { Post } from "@/types/post";
 
 export default async function HomePage() {
   const session = await auth();
   const user = session?.user;
 
-  const posts = [];
+  const posts: Post[] = [];
 
   if (!user) {
     redirect("/auth/login");
   }
 
   return (
-    <section className="flex p-5">
-      <div className="flex flex-col basis-3/4">
-        <StoryList userName={getNameByEmail(user.email)} />
+    <section className="w-full flex flex-col md:flex-row p-5 max-w-[850px]">
+      <div className="w-full basis-3/4 min-w-0">
+        <StoryList />
         <PostList posts={posts} />
       </div>
-      <div className="basis-1/4 sm:hidden md:block">
+      <div className="basis-1/4 ml:8">
         <AvatarLocalNav user={user} />
       </div>
     </section>
