2957722be7f3e23f73ced80022a2ff28f3822c11
lecto17 <khwoo_rpsit@naver.com>
2025-03-07 17:54:00 +0900

    chore: 북마크, 좋아요 표시 아이콘 컴포넌트 추가

---
+++ src/components/icons/BookMarkIcon.tsx
@@ -0,0 +1,7 @@
+import { CiBookmark } from "react-icons/ci";
+
+const BookMarkIcon = () => {
+  return <CiBookmark size={24} />;
+};
+
+export default BookMarkIcon;
---
+++ src/components/icons/HeartIcon.tsx
@@ -0,0 +1,7 @@
+import { CiHeart } from "react-icons/ci";
+
+const HeartIcon = () => {
+  return <CiHeart size={24} />;
+};
+
+export default HeartIcon;
