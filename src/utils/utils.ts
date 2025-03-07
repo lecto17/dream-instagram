84403cd06d0165c7745fd1ca686d5310196973b4
lecto17 <khwoo_rpsit@naver.com>
2025-03-07 17:56:06 +0900

    feat: 날짜 상대시간 표시 함수 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,12 @@
+import { format } from "timeago.js";
+
 export const getNameByEmail = (email?: string | null) => {
   if (!email) return "";
 
   if (!email.includes("@")) return email;
   return email.split("@")[0];
+};
+
+export const parseDate = (date: string) => {
+  return format(date);
 };
