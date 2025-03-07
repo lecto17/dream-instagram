78a970604dfbd8f991bef6de0619f3c8bf95d388
lecto17 <khwoo_rpsit@naver.com>
2025-03-07 17:52:27 +0900

    chore: route handler로 포스트 요청 방식 변경하여 과거 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,10 @@ import StoryList from "@/components/stories/StoryList";
 import PostList from "@/components/posts/PostList";
 import AvatarLocalNav from "@/components/navigation/AvatarLocalNav";
 import { redirect } from "next/navigation";
-import { Post } from "@/types/post";
 
 export default async function HomePage() {
   const session = await auth();
   const user = session?.user;
-
-  const posts: Post[] = [];
 
   if (!user) {
     redirect("/auth/login");
@@ -19,7 +16,7 @@ export default async function HomePage() {
     <section className="w-full flex flex-col md:flex-row p-5 max-w-[850px]">
       <div className="w-full basis-3/4 min-w-0">
         <StoryList />
-        <PostList posts={posts} />
+        <PostList />
       </div>
       <div className="basis-1/4 ml:8">
         <AvatarLocalNav user={user} />
