61b2d1d6a9fe8b05afc25c790dbcd036136c5b04
lecto17 <khwoo_rpsit@naver.com>
2025-03-07 11:53:49 +0900

    chore: post 이미지 불러올 때 사용할 sanity 이미지 빌더 추가

--- src/sanity/sanity.ts
+++ src/sanity/sanity.ts
@@ -1,5 +1,7 @@
 import { apiVersion, dataset, projectId } from "@/sanity/env";
 import { createClient } from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: projectId,
@@ -8,3 +10,9 @@ export const client = createClient({
   apiVersion: apiVersion, // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
   token: process.env.SANITY_SECRET_TOKEN, // Needed for certain operations like updating content, accessing drafts or using draft perspectives
 });
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source: SanityImageSource) => {
+  return builder.image(source).width(800).url();
+};
