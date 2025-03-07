064972dadc37710c935c2aff43da044fd90e3807
lecto17 <khwoo_rpsit@naver.com>
2025-03-07 11:54:21 +0900

    chore: post, 댓글 모델 타입 추가

--- src/types/post.ts
+++ src/types/post.ts
@@ -8,3 +8,24 @@ export type Post = {
   content: string;
   //   comments:
 };
+
+export type Comment = {
+  comment: string;
+  username: string;
+  image: string;
+};
+
+export type SimplePost = Omit<FullPost, "comment"> & {
+  comments: number;
+};
+
+export type FullPost = {
+  id: string;
+  username: string;
+  userImage: string;
+  image: string;
+  text: string;
+  likes: string;
+  createdAt: string;
+  comments: Comment[];
+};
