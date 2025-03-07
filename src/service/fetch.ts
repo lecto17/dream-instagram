bbe95fb5ba4e05e1a3d79de5d9cd46990929b214
lecto17 <khwoo_rpsit@naver.com>
2025-03-07 11:53:20 +0900

    feat: post 가져오는 GROQ 추가

--- src/service/fetch.ts
+++ src/service/fetch.ts
@@ -1,7 +1,7 @@
 import { client } from "@/sanity/sanity";
 
 type DataType = "USERS" | "AUTHORS" | "POSTS" | "CATEGORIES" | "FOLLOWINGS";
-type QueryType = "FOLLOWINGS" | "FOLLOWINGS1";
+type QueryType = "FOLLOWINGS" | "FOLLOWINGS_POSTS";
 
 export const getQuery = (queryType: QueryType, payload: string) => {
   let query = "";
@@ -15,6 +15,21 @@ export const getQuery = (queryType: QueryType, payload: string) => {
         followers[]-> {username,image},
         "bookmarks":bookmarks[]->id
       }`;
+      break;
+    case "FOLLOWINGS_POSTS":
+      const simplePostProjection = `
+        ...,
+        "username": author->username,
+        "userImage": author->image,
+        "likes": likes[]->username,
+        "text": comments[0].comment,
+        "comments": count(comments),
+        "id": _id,
+        "createdAt": _createdAt
+      `;
+      query = `*[_type == "post" && author->username == "${payload}"
+        || author._ref in *[_type == "user" && username == "${payload}"].following[]._ref] 
+        | order(_createdAt desc){${simplePostProjection}}`;
       break;
     default:
       break;
