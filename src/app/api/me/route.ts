fc80daea534890804e24db1b54e515ccbc23f45a
lecto17 <khwoo_rpsit@naver.com>
2025-03-07 17:52:56 +0900

    chore: 사용자 세션 검증 함수 추가

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,14 +1,17 @@
-import { auth } from "/auth";
+// import { auth } from "/auth";
 import { getNameByEmail } from "@/utils/utils";
 import { getUserByname } from "@/service/user";
+import { validateSession } from "@/actions/action";
 
 export async function GET() {
-  const session = await auth();
-  const user = session?.user;
+  // const session = await auth();
+  // const user = session?.user;
 
-  if (!user) {
-    return new Response("[Error] UnAuthorized", { status: 401 });
-  }
+  // if (!user) {
+  //   return new Response("[Error] UnAuthorized", { status: 401 });
+  // }
+
+  const user = await validateSession();
 
   const data = await getUserByname(getNameByEmail(user.email));
   return new Response(JSON.stringify(data), { status: 200 });
