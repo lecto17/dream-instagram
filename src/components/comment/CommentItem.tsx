b415eff0e4999dda696dd2e2d1c508212bbec606
lecto17 <khwoo_rpsit@naver.com>
2025-03-12 15:15:55 +0900

    feat: 아바타, 댓글 함께있는 컴포넌트 추가

---
+++ src/components/comment/CommentItem.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Avatar from "@/components/avatar/Avatar";
+import { Comment } from "@/types/post";
+
+type CommentItemProps = {
+  comment: Comment;
+};
+
+const CommentItem = ({
+  comment: { username, image, comment },
+}: CommentItemProps) => {
+  return (
+    <div className="flex">
+      <Avatar user={{ username, image }} size="xs" />
+      <p className="flex items-center ml-1">
+        <span className="font-bold mr-2">{username}</span>
+        {comment}
+      </p>
+    </div>
+  );
+};
+
+export default CommentItem;
