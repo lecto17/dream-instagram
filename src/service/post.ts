652831948d993bce3f29ec26631f73986fd8f349
lecto17 <khwoo_rpsit@naver.com>
2025-03-12 13:49:23 +0900

    feat: 포스트 댓글 목록 가져오는 service 추가

--- src/service/post.ts
+++ src/service/post.ts
@@ -1,11 +1,19 @@
 import { client, urlFor } from "@/sanity/sanity";
 import { getQuery } from "@/service/fetch";
-import { SimplePost } from "@/types/post";
+import { FullPost, SimplePost } from "@/types/post";
 
 export const getFollowingsPost = (name: string) => {
   return client
     .fetch<SimplePost[]>(getQuery("FOLLOWINGS_POSTS", name))
     .then((posts) => {
       return posts.map((post) => ({ ...post, image: urlFor(post.image) }));
+    });
+};
+
+export const getPostComments = (id: string) => {
+  return client
+    .fetch<FullPost>(getQuery("POST_DETAIL", id))
+    .then((comments) => {
+      return comments;
     });
 };
