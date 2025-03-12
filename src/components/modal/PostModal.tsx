720ed3f4c25ce5b91a5d1cb75879ce285070a1db
lecto17 <khwoo_rpsit@naver.com>
2025-03-12 15:16:29 +0900

    chore: 모달 Frame 컴포넌트 추가

---
+++ src/components/modal/PostModal.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useEffect } from "react";
+
+type PostModal = {
+  children: React.ReactNode;
+  onClose: () => void;
+};
+
+const PostModal = ({ children, onClose }: PostModal) => {
+  const handleClickOuter = (e: React.MouseEvent<HTMLElement>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") onClose();
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [onClose]);
+
+  return (
+    <section
+      className="w-full h-full fixed top-0 left-0 z-[99999] bg-black bg-opacity-65 flex justify-center items-center"
+      onClick={handleClickOuter}
+    >
+      {children}
+    </section>
+  );
+};
+
+export default PostModal;
