1ba47ce29bf9cd6bab11848a9bb6ece0b261a8cf
lecto17 <khwoo_rpsit@naver.com>
2025-03-13 12:12:47 +0900

    feat: 사용자 검색 결과 아이템 컴포넌트 추가

---
+++ src/components/search/SearchUserAvatar.tsx
@@ -0,0 +1,32 @@
+import Avatar from "@/components/avatar/Avatar";
+import { SearchUser } from "@/types/user";
+import Link from "next/link";
+
+type Props = {
+  user: SearchUser;
+};
+
+const SearchUserAvatar = ({
+  user: { followers, following, image, name, username },
+}: Props) => {
+  return (
+    <li className="border border-gray-300 w-full p-5 mb-2 bg-white hover:bg-neutral-50">
+      <Link
+        href={`/users/${username}`}
+        className="w-full flex justify-center items-center"
+      >
+        <Avatar user={{ username, image }} size="big" />
+        <div className="flex flex-col w-full leading-4 ml-3 text-gray-400">
+          <span className="text-black">{username}</span>
+          <span>{name}</span>
+          <p className="w-full flex">
+            <span>{followers} followers</span>
+            <span>{following} following</span>
+          </p>
+        </div>
+      </Link>
+    </li>
+  );
+};
+
+export default SearchUserAvatar;
