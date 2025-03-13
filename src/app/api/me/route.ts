95d75771392caa4a5fbed8d2539bba6986774bd2
lecto17 <khwoo_rpsit@naver.com>
2025-03-13 12:07:33 +0900

    chore: 목적에 맞는 함수명으로 변경

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,18 +1,10 @@
-// import { auth } from "/auth";
 import { getNameByEmail } from "@/utils/utils";
-import { getUserByname } from "@/service/user";
+import { getFollowingsByUserName } from "@/service/user";
 import { validateSession } from "@/actions/action";
 
 export async function GET() {
-  // const session = await auth();
-  // const user = session?.user;
-
-  // if (!user) {
-  //   return new Response("[Error] UnAuthorized", { status: 401 });
-  // }
-
   const user = await validateSession();
+  const data = await getFollowingsByUserName(getNameByEmail(user.email));
 
-  const data = await getUserByname(getNameByEmail(user.email));
   return new Response(JSON.stringify(data), { status: 200 });
 }
