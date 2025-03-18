86d3db0294d4a55d9f6cf30202dd044276d8b404
lecto17 <khwoo_rpsit@naver.com>
2025-03-18 17:06:18 +0900

    chore: filled 북마크 컴포넌트 추가

---
+++ src/components/icons/BookMarkIconFilled.tsx
@@ -0,0 +1,8 @@
+import { ICON_SIZE } from "@/constants/size";
+import { RiBookmarkFill } from "react-icons/ri";
+
+const BookMarkIconFilled = () => {
+  return <RiBookmarkFill size={ICON_SIZE} />;
+};
+
+export default BookMarkIconFilled;
