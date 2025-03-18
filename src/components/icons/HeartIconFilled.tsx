8145987a2fcc6bb9ca1e331be5b990223bdcca0d
lecto17 <khwoo_rpsit@naver.com>
2025-03-18 17:06:32 +0900

    chore: filled 하트 아이콘 컴포넌트 추가

---
+++ src/components/icons/HeartIconFilled.tsx
@@ -0,0 +1,8 @@
+import { ICON_SIZE } from "@/constants/size";
+import { AiFillHeart } from "react-icons/ai";
+
+const HeartIconFilled = () => {
+  return <AiFillHeart size={ICON_SIZE} color="red" id="red" />;
+};
+
+export default HeartIconFilled;
