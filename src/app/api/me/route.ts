13739c79ab9a23b555b56d45d3bf56ba742e4f45
lecto17 <khwoo_rpsit@naver.com>
2025-03-19 15:13:37 +0900

    feat: 북마크 처리 route handler 추가

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -1,10 +1,35 @@
 import { getNameByEmail } from "@/utils/utils";
-import { getFollowingsByUserName } from "@/service/user";
+import {
+  addBookMarkOnPost,
+  getFollowingsByUserName,
+  removeBookMarkOnPost,
+} from "@/service/user";
 import { validateSession } from "@/actions/action";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
   const user = await validateSession();
   const data = await getFollowingsByUserName(getNameByEmail(user.email));
 
   return new Response(JSON.stringify(data), { status: 200 });
+}
+
+export async function PUT(req: NextRequest) {
+  const user = await validateSession();
+
+  if (!user) {
+    return new Response("Authenticated Error");
+  }
+
+  const { postId, bookmark } = await req.json();
+
+  if (!postId || bookmark === undefined) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  const request = bookmark ? addBookMarkOnPost : removeBookMarkOnPost;
+
+  return request(postId, user.id)
+    .then(NextResponse.json)
+    .catch((err) => new Response(JSON.stringify(err), { status: 500 }));
 }
