5ee9c2ddaf39a6b13723145bb6e85df0e27a47f9
lecto17 <khwoo_rpsit@naver.com>
2025-03-21 13:07:17 +0900

    chore: hook 내 함수 메모이제이션 추가

--- src/hooks/useComment.ts
+++ src/hooks/useComment.ts
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import { Comment } from "@/types/post";
+import { useCallback } from "react";
 
 export default function useComment(postId: string) {
   const {
@@ -8,35 +9,45 @@ export default function useComment(postId: string) {
     mutate,
   } = useSWR<Comment[]>(`api/posts/${postId}`);
 
-  const updateComment = async (comment: Comment) => {
-    return fetch(`/api/posts/${postId}`, {
-      method: "PUT",
-      body: JSON.stringify({ postId, comment }),
-    }).then((res) => res.json());
-  };
+  // usePosts의 updatePostLike에는 useCallback을 감싸주지 않음.
+  // 해당 함수를 호출할 때마다 새롭게 전달되는 매개변수를 전달하고 이걸 기반으로 api 요청을 하기에 useCallback 필요없음.
+  // updateComment 같은 경우는 함수 호출마다 comment를 새롭게 전달받긴 하지만, 이 함수 외부 변수인 postId에 의존하고 있는데,
+  // 이 변수가 바뀌지 않는다면 함수는 그대로 사용해도 괜찮음. 그렇기에 useCallback 감아줌.
+  const updateComment = useCallback(
+    async (comment: Comment) => {
+      return fetch(`/api/posts/${postId}`, {
+        method: "PUT",
+        body: JSON.stringify({ postId, comment }),
+      }).then((res) => res.json());
+    },
+    [postId]
+  );
 
-  const setComment = (comment: Comment) => {
-    let newComments;
-    if (comment?.id) {
-      newComments = comments?.map(({ id, ...rest }) =>
-        id === comment.id
-          ? { id, ...rest, comment: comment.comment }
-          : { id, ...rest }
-      );
-    } else {
-      newComments = [
-        ...(comments?.length ? comments : []),
-        { ...comment, id: "" },
-      ];
-    }
+  const setComment = useCallback(
+    (comment: Comment) => {
+      let newComments;
+      if (comment?.id) {
+        newComments = comments?.map(({ id, ...rest }) =>
+          id === comment.id
+            ? { id, ...rest, comment: comment.comment }
+            : { id, ...rest }
+        );
+      } else {
+        newComments = [
+          ...(comments?.length ? comments : []),
+          { ...comment, id: "" },
+        ];
+      }
 
-    mutate(updateComment(comment), {
-      optimisticData: newComments,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+      mutate(updateComment(comment), {
+        optimisticData: newComments,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [comments, mutate, updateComment]
+  );
 
   return { comments, isLoading, setComment };
 }
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import { DetailUser } from "@/types/user";
+import { useCallback } from "react";
 
 export default function useUser() {
   const { data: user, isLoading, mutate } = useSWR<DetailUser>("/api/me");
@@ -11,21 +12,24 @@ export default function useUser() {
     }).then((res) => res.json());
   };
 
-  const setBookMarked = (postId: string, bookmark: boolean) => {
-    const newUser = {
-      ...user!,
-      bookmarks: bookmark
-        ? [...(user?.bookmarks || []), postId]
-        : user?.bookmarks?.filter((id) => id != postId) || [],
-    };
+  const setBookMarked = useCallback(
+    (postId: string, bookmark: boolean) => {
+      const newUser = {
+        ...user!,
+        bookmarks: bookmark
+          ? [...(user?.bookmarks || []), postId]
+          : user?.bookmarks?.filter((id) => id != postId) || [],
+      };
 
-    mutate(updateBookMark(postId, bookmark), {
-      optimisticData: newUser,
-      populateCache: false,
-      revalidate: false,
-      rollbackOnError: true,
-    });
-  };
+      mutate(updateBookMark(postId, bookmark), {
+        optimisticData: newUser,
+        populateCache: false,
+        revalidate: false,
+        rollbackOnError: true,
+      });
+    },
+    [user, mutate]
+  );
   // const setFollowing = (postId: string, bookmark: boolean) => {};
 
   return { user, isLoading, setBookMarked };
