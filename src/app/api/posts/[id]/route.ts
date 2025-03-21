f22701fb334fd78da8e8660511a9eca1f269718a
lecto17 <khwoo_rpsit@naver.com>
2025-03-21 11:39:41 +0900

    feat: comment 추가, 업데이트 route handler 추가

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -1,5 +1,6 @@
 import { validateSession } from "@/actions/action";
-import { getPostComments } from "@/service/post";
+import { addComment, getPostComments, updateComment } from "@/service/post";
+import { NextRequest, NextResponse } from "next/server";
 
 type Context = {
   id: string;
@@ -14,4 +15,24 @@ export async function GET(
   const data = await getPostComments(id);
 
   return new Response(JSON.stringify(data), { status: 200 });
+}
+
+export async function PUT(req: NextRequest) {
+  const user = await validateSession();
+
+  if (!user) {
+    return new Response("Authenticated Error");
+  }
+
+  const { postId, comment } = await req.json();
+
+  if (!postId || !comment || !Object.keys(comment).length) {
+    return new Response("Bad Request", { status: 400 });
+  }
+
+  const request = comment?.id ? updateComment : addComment;
+
+  return request(postId, user.id, comment)
+    .then(NextResponse.json)
+    .catch((err) => new Response(JSON.stringify(err), { status: 500 }));
 }
