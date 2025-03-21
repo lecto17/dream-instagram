72e878695de3131d7337ef1f943b0d2821141402
lecto17 <khwoo_rpsit@naver.com>
2025-03-21 11:40:35 +0900

    feat: 댓글 입력 시 api 요청 작업

--- src/components/comment/CommentForm.tsx
+++ src/components/comment/CommentForm.tsx
@@ -1,19 +1,40 @@
+import useComment from "@/hooks/useComment";
+import { useCallback, useState } from "react";
+
 type CommentFormProps = {
+  postId: string;
   formStyle?: string;
 };
 
-const CommentForm = ({ formStyle }: CommentFormProps) => {
+const CommentForm = ({ postId, formStyle }: CommentFormProps) => {
+  const [value, setValue] = useState("");
+  const { setComment } = useComment(postId);
+
+  const handleChangeValue = useCallback((e: React.ChangeEvent) => {
+    const comment = (e.target as HTMLInputElement).value;
+    setValue(comment);
+  }, []);
+
+  const handleSubmit = (e?: React.FormEvent) => {
+    e?.preventDefault();
+    setComment({ comment: value, username: "" });
+    setValue("");
+  };
+
   return (
-    <form className={`flex w-full ${formStyle}`}>
+    <form className={`flex w-full ${formStyle}`} onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="Add a comment..."
-        className="w-full py-1 px-4 outline-none text-sm"
+        className="w-full py-2 pr-4 outline-none text-sm"
+        value={value}
+        onChange={handleChangeValue}
       />
       <input
         type="submit"
-        value="등록"
-        className="py-1 px-2 text-sky-400 font-bold hover:text-black rounded-md text-sm cursor-pointer transition-all duration-300"
+        value="게시"
+        className={`py-1 px-2 font-bold hover:text-black rounded-md text-sm cursor-pointer transition-all duration-300 ${value.length ? "text-sky-400" : "text-gray-400"}`}
+        disabled={!value}
       />
     </form>
   );
