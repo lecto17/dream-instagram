7d29e686de17b95651ddc041651ae6bcf16eacac
lecto17 <khwoo_rpsit@naver.com>
2025-03-22 15:10:19 +0900

    feat: 프로필 페이지 내 follow 버튼 컴포넌트 추가

--- src/components/user/UserProfileHome.tsx
+++ src/components/user/UserProfileHome.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Avatar from "@/components/avatar/Avatar";
+import FollowButton from "@/components/button/FollowButton";
 import Loading from "@/components/loading/Loading";
 import UserProfileTab from "@/components/user/UserProfileTab";
+import useUser from "@/hooks/useUser";
 import { UserProfile } from "@/types/user";
 import useSWR from "swr";
 
@@ -15,11 +17,21 @@ const UserProfileHome = ({ propUserName }: Props) => {
     `/api/users/${propUserName}`
   );
 
+  const { user } = useUser();
+
   if (isLoading) return <>{isLoading && <Loading />}</>;
-  if (!data || !Object.keys(data).length)
+  if (!data || !Object.keys(data).length || !user || !Object.keys(user).length)
     return <>사용자가 존재하지 않습니다.</>;
 
-  const { image, following, followers, username, name, posts } = data;
+  // 로그인한 유저의 following
+  const { following, id: loginUserId } = user;
+  // Profile user의 정보
+  const { id: profileUserId, image, followers, username, name, posts } = data;
+
+  const isFollow =
+    following?.some(
+      ({ id: followingUserId }) => profileUserId === followingUserId
+    ) ?? false;
 
   return (
     <>
@@ -28,15 +40,23 @@ const UserProfileHome = ({ propUserName }: Props) => {
       <div className="flex flex-col w-full items-center">
         <section className="flex w-full justify-center items-center py-8 mb-10">
           <Avatar user={{ username, image }} size="ultra" />
-          <div className="flex flex-col ml-7 space-y-2">
+          <div className="flex flex-col ml-7 space-y-2 mr-2">
             <h1 className="text-2xl">{username}</h1>
             <p className="flex space-x-3 text-sm">
               <span className="font-bold">{posts}</span> &nbsp;posts
-              <span className="font-bold">{following}</span> &nbsp;following
+              <span className="font-bold">{following?.length}</span>{" "}
+              &nbsp;following
               <span className="font-bold">{followers}</span> &nbsp;followers
             </p>
             <p className="text-2xl font-bold">{name}</p>
           </div>
+          {loginUserId !== profileUserId && (
+            <FollowButton
+              profileUserId={profileUserId || ""}
+              profileUserName={propUserName}
+              isFollow={isFollow}
+            />
+          )}
         </section>
         <UserProfileTab propUserName={propUserName} />
       </div>
