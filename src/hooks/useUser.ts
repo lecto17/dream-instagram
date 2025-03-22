fc4cf055cd80f4fef8ebc82500be36bf08b63a1b
lecto17 <khwoo_rpsit@naver.com>
2025-03-22 15:10:56 +0900

    feat: useUser 훅 내 following 기능 추가

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,9 +1,10 @@
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import { DetailUser } from "@/types/user";
 import { useCallback } from "react";
 
 export default function useUser() {
   const { data: user, isLoading, mutate } = useSWR<DetailUser>("/api/me");
+  const { mutate: globalMutate } = useSWRConfig();
 
   const updateBookMark = async (postId: string, bookmark: boolean) => {
     return await fetch("/api/me", {
@@ -30,7 +31,33 @@ export default function useUser() {
     },
     [user, mutate]
   );
-  // const setFollowing = (postId: string, bookmark: boolean) => {};
 
-  return { user, isLoading, setBookMarked };
+  const updateFollowing = async (
+    profileUserId: string,
+    addOnFollowing: boolean
+  ) => {
+    return await fetch("/api/me/following", {
+      method: "PUT",
+      body: JSON.stringify({ profileUserId, addOnFollowing }),
+    }).then((res) => res.json());
+  };
+
+  const setFollowing = (
+    profileUserId: string,
+    profileUserName: string,
+    addOnFollowing: boolean
+  ) => {
+    const newFollowings = addOnFollowing
+      ? [...(user?.following || [])]
+      : (user?.following?.filter(({ id }) => id !== profileUserId) ?? []);
+
+    mutate(updateFollowing(profileUserId, addOnFollowing), {
+      optimisticData: { ...user!, following: newFollowings },
+      populateCache: false,
+      revalidate: false,
+      rollbackOnError: true,
+    }).then(() => globalMutate(`/api/users/${profileUserName}`));
+  };
+
+  return { user, isLoading, setBookMarked, setFollowing };
 }
