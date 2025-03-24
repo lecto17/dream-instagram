ae516b022b167318436fa99b355aae9ea2467717
lecto17 <khwoo_rpsit@naver.com>
2025-03-24 17:20:53 +0900

    feat: 격자 형식 포스트 컴포넌트 생성

---
+++ src/components/posts/PostGrid.tsx
@@ -0,0 +1,27 @@
+import Loading from "@/components/loading/Loading";
+import PostGridCard from "@/components/posts/PostGridCard";
+import { SimplePost } from "@/types/post";
+import useSWR from "swr";
+
+type Props = {
+  username: string;
+  tab: "posts" | "saved" | "liked";
+};
+
+const PostGrid = ({ username, tab }: Props) => {
+  const { data: posts, isLoading } = useSWR<SimplePost[]>(
+    `/api/users/${username}/${tab}`
+  );
+
+  return (
+    <section className="flex flex-col">
+      {isLoading && <Loading />}
+      <ul className="w-full mx-auto grid gap-2 grid-cols-3">
+        {posts?.map((post) => <PostGridCard key={post.id} post={post} />)}
+      </ul>
+      {(!posts?.length || !posts) && <div>No-data</div>}
+    </section>
+  );
+};
+
+export default PostGrid;
