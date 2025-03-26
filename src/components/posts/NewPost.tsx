3b446870db973cfebcff552902272b7779894ec5
lecto17 <khwoo_rpsit@naver.com>
2025-03-26 17:36:15 +0900

    feat: 포스트 작성 기능 추가

--- src/components/posts/NewPost.tsx
+++ src/components/posts/NewPost.tsx
@@ -3,21 +3,52 @@
 import Avatar from "@/components/avatar/Avatar";
 import PublishButton from "@/components/button/PublishButton";
 import FileUpload from "@/components/input/FileUpload";
+import Loading from "@/components/loading/Loading";
+import usePosts from "@/hooks/usePosts";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 const NewPost = () => {
   const { data } = useSession();
   const user = data?.user;
+  const router = useRouter();
 
-  const [text, setText] = useState<string>("");
   const [file, setFile] = useState<File | undefined>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string>();
 
-  if (!user) redirect("/auth/login");
+  const { addPost } = usePosts();
+
+  if (!user) {
+    router.push("/auth/login");
+    return;
+  }
+
+  const handleClickPublish = async () => {
+    setLoading(true);
+    try {
+      await addPost(textAreaRef.current?.value || "", file || undefined);
+    } catch (err: unknown) {
+      if (err instanceof Error) setError(err.toString());
+    } finally {
+      router.push("/");
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex flex-col p-4 items-center">
+      {loading && (
+        <div className="absolute z-20 inset-0 w-full h-full bg-sky-400/20">
+          <Loading />
+        </div>
+      )}
+      {error && (
+        <p className="w-full h-full bg-red-300 p-4 font-bold text-center mb-5">
+          asdf
+        </p>
+      )}
       <div className="flex items-center mb-5">
         <Avatar user={user} />
         <span className="ml-3">{user.username}</span>
@@ -25,11 +56,9 @@ const NewPost = () => {
       <FileUpload file={file} onChange={setFile} />
       <textarea
         className="w-full border px-4 py-2 my-5 min-h-32 outline-none text-sm resize-none"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
         placeholder="write a message..."
       />
-      <PublishButton text={text} file={file} />
+      <PublishButton onClick={handleClickPublish} />
     </div>
   );
 };
