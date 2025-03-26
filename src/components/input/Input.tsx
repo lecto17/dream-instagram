b6e172d1402f578115a9298955f18b9241498285
lecto17 <khwoo_rpsit@naver.com>
2025-03-26 17:39:16 +0900

    chore: input 컴포넌트 placeholder 외부에서 주입하도록 수정

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -3,14 +3,15 @@
 type Props = {
   propValue?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
 };
 
-const Input = ({ propValue, onChange }: Props) => {
+const Input = ({ propValue, onChange, placeholder }: Props) => {
   return (
     <section>
       <input
         className="w-full px-3 py-2 border border-black rounded-sm outline-none mt-3"
-        placeholder="유저명 혹은 이름으로 검색해보세요."
+        placeholder={placeholder}
         value={propValue}
         onChange={onChange}
       />
--- src/components/search/SearchHome.tsx
+++ src/components/search/SearchHome.tsx
@@ -3,6 +3,8 @@ import Input from "@/components/input/Input";
 import SearchResult from "@/components/search/SearchResult";
 import { useState } from "react";
 
+const placeholder = "유저명 혹은 이름으로 검색해보세요.";
+
 const SearchHome = () => {
   const [value, setValue] = useState<string>("");
 
@@ -14,7 +16,11 @@ const SearchHome = () => {
 
   return (
     <div className="flex flex-col space-y-5">
-      <Input propValue={value} onChange={handleChangeValue} />
+      <Input
+        propValue={value}
+        onChange={handleChangeValue}
+        placeholder={placeholder}
+      />
       <SearchResult text={value} />
     </div>
   );
