961c5abd847448ca7004924886136bbf35fb3b60
lecto17 <khwoo_rpsit@naver.com>
2025-03-28 13:27:54 +0900

    chore: loading 컴포넌트 pulse type 추가

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -1,20 +1,21 @@
 import GridSpinner from "@/components/spinner/GridSpinner";
 import { LOADING_BAR_COLOR } from "@/constants/color";
+import { PulseLoader } from "react-spinners";
 
-type LoadingType = "Grid";
+type LoadingType = "Grid" | "Pulse";
 type Props = {
   type?: LoadingType;
   color?: string;
+  style?: string;
 };
 
-const Loading = ({ type = "Grid", color }: Props) => {
-  if (type === "Grid") {
-    return (
-      <div className="w-full flex justify-center mt-32">
-        <GridSpinner color={color ?? LOADING_BAR_COLOR} />
-      </div>
-    );
-  }
+const Loading = ({ type = "Grid", color, style }: Props) => {
+  return (
+    <div className={`w-full flex justify-center mt-32 ${style}`}>
+      {type === "Grid" && <GridSpinner color={color ?? LOADING_BAR_COLOR} />}
+      {type === "Pulse" && <PulseLoader color={color ?? LOADING_BAR_COLOR} />}
+    </div>
+  );
 };
 
 export default Loading;
