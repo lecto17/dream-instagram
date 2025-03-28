7bac19fad2c7d17c7d82d1c28f884f2582874165
lecto17 <khwoo_rpsit@naver.com>
2025-03-28 13:11:26 +0900

    feat: 존재하는 user 확인 후 컴포넌트 출력하도록 작업

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -1,15 +1,24 @@
 import UserProfileHome from "@/components/user/UserProfileHome";
+import UserProfileTab from "@/components/user/UserProfileTab";
+import { getUserAllInformation } from "@/service/user";
+import { notFound } from "next/navigation";
 
 type Props = {
   params: Promise<{ username: string }>;
 };
 
+export const revalidate = 0;
+
 const page = async ({ params }: Props) => {
   const { username } = await params;
+  const user = await getUserAllInformation(username);
+
+  if (!user) notFound();
 
   return (
     <div className="w-full max-w-[850px]">
-      <UserProfileHome propUserName={username} />
+      <UserProfileHome username={username} />
+      <UserProfileTab username={username} />
     </div>
   );
 };
