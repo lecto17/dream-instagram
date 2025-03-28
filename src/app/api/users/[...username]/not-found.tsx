97658dc5e9d7767d5cd7b3b6cdd0579bdeedbec0
lecto17 <khwoo_rpsit@naver.com>
2025-03-28 12:14:23 +0900

    chore: 존재하지 않는 user page 접속 시 출력할 컴포넌트 생성

--- src/app/api/users/[...username]/not-found.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-  username?: string;
-};
-const NotFound = ({ username }: Props) => {
-  return <p>{`${username}라는 사용자가 존재하지 않습니다.`}</p>;
-};
-
-export default NotFound;
---
+++ src/app/users/[username]/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full flex justify-center mt-40 text-3xl">
+      <p>{`해당 사용자를 찾을 수가 없습니다.🥲`}</p>
+    </div>
+  );
+};
+
+export default NotFound;
