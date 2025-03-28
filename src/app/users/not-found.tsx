b0a1b1745d8977bbecb87262c8ee52a11223a85f
lecto17 <khwoo_rpsit@naver.com>
2025-03-28 12:14:50 +0900

    chore: 존재하지 않는 page 접속 시 출력할 컴포넌트 생성

---
+++ src/app/users/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="w-full h-full flex justify-center mt-40 text-3xl">
+      {"Page Not Found"}
+    </div>
+  );
+};
+
+export default NotFound;
