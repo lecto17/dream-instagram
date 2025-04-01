f1b646f39a8642d7708601a0e08c27cae4203ebf
lecto17 <khwoo_rpsit@naver.com>
2025-04-01 17:18:41 +0900

    chore: disable cache

--- src/sanity/sanity.ts
+++ src/sanity/sanity.ts
@@ -6,7 +6,7 @@ import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 export const client = createClient({
   projectId: projectId,
   dataset: dataset,
-  useCdn: true, // set to `false` to bypass the edge cache
+  useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: apiVersion, // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
   token: process.env.SANITY_SECRET_TOKEN, // Needed for certain operations like updating content, accessing drafts or using draft perspectives
 });
