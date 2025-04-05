e4424b5ca81876f4574fb305f66b4bda50af332e
lecto17 <khwoo_rpsit@naver.com>
2025-04-05 18:15:16 +0900

    chore: 캐시 옵션에 따른 sanity client 분리

--- src/sanity/sanity.ts
+++ src/sanity/sanity.ts
@@ -3,12 +3,21 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-export const client = createClient({
+const clientOption = {
   projectId: projectId,
   dataset: dataset,
-  useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: apiVersion, // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
   token: process.env.SANITY_SECRET_TOKEN, // Needed for certain operations like updating content, accessing drafts or using draft perspectives
+};
+
+export const client = createClient({
+  ...clientOption,
+  useCdn: false, // set to `false` to bypass the edge cache
+});
+
+export const freshClient = createClient({
+  ...clientOption,
+  useCdn: true,
 });
 
 const builder = imageUrlBuilder(client);
