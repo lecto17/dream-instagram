a9d988d53a377af2af20956ac297920abd689e61
lecto17 <khwoo_rpsit@naver.com>
2025-04-08 11:48:36 +0900

    chore: posts 조회 시 사용자 email로 검색하도록 수정

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,10 +1,12 @@
 import { validateSession } from "@/actions/action";
 import { getFollowingsPost } from "@/service/post";
+import { findUserIdBy } from "@/service/user";
 
 export async function GET() {
   const user = await validateSession();
   if (!user) return new Response("not loggined", { status: 403 });
   // const data = await getFollowingsPost(getNameByEmail(user.email));
-  const data = await getFollowingsPost(user.id);
+  const { id } = await findUserIdBy(user.email);
+  const data = await getFollowingsPost(id);
   return new Response(JSON.stringify(data), { status: 200 });
 }
