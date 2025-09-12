18749cbaa265d865f1398d7c68a4a170875b8179
lecto17 <khwoo_rpsit@naver.com>
2025-09-12 16:07:16 +0900

    chore: user 개인 profile 전달되도록 수정

--- src/app/api/me/route.ts
+++ src/app/api/me/route.ts
@@ -2,34 +2,41 @@ import {
   addBookMarkOnPost,
   getFollowingsBy,
   removeBookMarkOnPost,
-} from "@/service/user";
-import { validateSession } from "@/actions/action";
-import { NextRequest, NextResponse } from "next/server";
+} from '@/service/user';
+import {
+  getAuthenticatedUser,
+  // validateSession
+} from '@/actions/action';
+import { NextRequest, NextResponse } from 'next/server';
+import { getMyProfile } from '@/service/supa-user';
+
+// export async function GET() {
+//   const user = await validateSession();
+//   if (!user) return new Response('UnAthenticated Error');
+
+//   const data = await getFollowingsBy(user.email || '');
+//   return new Response(JSON.stringify(data), { status: 200 });
+// }
 
 export async function GET() {
-  const user = await validateSession();
-  if (!user) return new Response("UnAthenticated Error");
+  const user = await getAuthenticatedUser();
+  if (!user) return new Response('UnAthenticated Error');
 
-  const data = await getFollowingsBy(user.email || "");
-  return new Response(JSON.stringify(data), { status: 200 });
+  const profile = await getMyProfile(user.id);
+  return new Response(JSON.stringify(profile), { status: 200 });
 }
 
 export async function PUT(req: NextRequest) {
-  const user = await validateSession();
-
-  if (!user) {
-    return new Response("Authenticated Error");
-  }
-
-  const { postId, bookmark } = await req.json();
-
-  if (!postId || bookmark === undefined) {
-    return new Response("Bad Request", { status: 400 });
-  }
-
-  const request = bookmark ? addBookMarkOnPost : removeBookMarkOnPost;
-
-  return request(postId, user.id)
-    .then(NextResponse.json)
-    .catch((err) => new Response(JSON.stringify(err), { status: 500 }));
+  // const user = await validateSession();
+  // if (!user) {
+  //   return new Response('Authenticated Error');
+  // }
+  // const { postId, bookmark } = await req.json();
+  // if (!postId || bookmark === undefined) {
+  //   return new Response('Bad Request', { status: 400 });
+  // }
+  // const request = bookmark ? addBookMarkOnPost : removeBookMarkOnPost;
+  // return request(postId, user.id)
+  //   .then(NextResponse.json)
+  //   .catch((err) => new Response(JSON.stringify(err), { status: 500 }));
 }
