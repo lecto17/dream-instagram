fc54baa2f9a00acbef4c2460b6c12e839024674a
lecto17 <khwoo_rpsit@naver.com>
2025-09-16 14:26:40 +0900

    chore: post 데이터 없는 경우 UI 추가

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -3,10 +3,15 @@ import { LOADING_BAR_COLOR } from '@/constants/color';
 import PostCard from '@/components/posts/PostCard';
 import GridSpinner from '@/components/spinner/GridSpinner';
 import usePosts from '@/hooks/usePosts';
+import { useSearchParams } from 'next/navigation';
+import { getDateYYYYMMDDWithDash } from '@/utils/utils';
 // import RecommendUsers from "@/components/user/RecommendUsers";
 
 const PostList = () => {
-  const { posts, isLoading, addCommentOnPost } = usePosts();
+  const date =
+    useSearchParams().get('date') ||
+    getDateYYYYMMDDWithDash().replaceAll('-', '');
+  const { posts, isLoading, addCommentOnPost } = usePosts(date);
 
   if (isLoading) {
     return (
@@ -21,9 +26,8 @@ const PostList = () => {
   // }
 
   return (
-    <ul className="flex flex-col items-center">
-      {posts != null &&
-        posts.length > 0 &&
+    <ul className="flex flex-col items-center h-full min-h-full overflow-y-auto p-5 space-y-10">
+      {posts != null && posts.length > 0 ? (
         posts.map((post, idx) => (
           <li key={post.id}>
             <PostCard
@@ -33,7 +37,14 @@ const PostList = () => {
               addCommentOnPost={addCommentOnPost}
             />
           </li>
-        ))}
+        ))
+      ) : (
+        <div className="w-full flex justify-center mt-32">
+          <p className="text-gray-500 whitespace-pre-line">
+            {'해당 일자에 게시글이 없습니다.\n\n먼저 게시글을 등록해보세요 🙂'}
+          </p>
+        </div>
+      )}
     </ul>
   );
 };
