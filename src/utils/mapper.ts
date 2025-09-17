4b693660dc35463171b08df6a963e15ca57b1b04
lecto17 <khwoo_rpsit@naver.com>
2025-09-17 17:59:29 +0900

    feat: api response 재귀적으로 key값 camelCase로 변환 작업

--- src/utils/mapper.ts
+++ src/utils/mapper.ts
@@ -23,7 +23,17 @@ const transformKeys = (
 
   for (const [key, value] of Object.entries(obj)) {
     const pascalKey = snakeToCamel(key);
-    transformed[pascalKey] = value;
+    if (
+      value &&
+      typeof value === 'object' &&
+      !Array.isArray(value) &&
+      !(value instanceof Date)
+    ) {
+      // 중첩된 객체에 대해 재귀적으로 transformKeys 적용
+      transformed[pascalKey] = transformKeys(value as Record<string, unknown>);
+    } else {
+      transformed[pascalKey] = value;
+    }
   }
 
   return transformed;
