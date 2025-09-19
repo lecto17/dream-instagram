560bd9ab3a01b36dad7cbfc82b8e655bd453b21f
lecto17 <khwoo_rpsit@naver.com>
2025-09-19 14:33:44 +0900

    chore: GNB 메뉴 드랍 다운 형태로 변경

---
+++ src/components/dropdown/Dropdown.tsx
@@ -0,0 +1,28 @@
+type DropdownProps = {
+  options: {
+    label: React.ReactNode;
+    value: string;
+    onClick?: (data?: unknown) => void;
+  }[];
+};
+
+const Dropdown = ({ options }: DropdownProps) => {
+  return (
+    <div className="bg-white rounded-md shadow-md min-w-20">
+      <ul className="flex flex-col gap-2">
+        {options.map((option) => (
+          <li key={option.value}>
+            <button
+              className="w-full p-2 text-sm hover:bg-gray-300 transition whitespace-nowrap"
+              onClick={option?.onClick}
+            >
+              {option.label}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Dropdown;
