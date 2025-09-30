b8a702559a0364dd68007f4d70d4c2bfcd579c7a
lecto17 <khwoo_rpsit@naver.com>
2025-09-30 12:39:37 +0900

    refactor: nodejs 환경에서 해당 라우트 핸들러 실행되도록 지정

--- src/app/api/channels/[channelId]/check-password/route.ts
+++ src/app/api/channels/[channelId]/check-password/route.ts
@@ -1,3 +1,5 @@
+export const runtime = 'nodejs';
+
 import { NextRequest, NextResponse } from 'next/server';
 import { getAuthenticatedUser } from '@/actions/action';
 import { serverSupabase } from '@/lib/supabaseServerClient';
