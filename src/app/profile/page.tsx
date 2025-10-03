114e38317a540ba465e83f33ea47857d73295f27
lecto17 <khwoo_rpsit@naver.com>
2025-10-04 08:28:17 +0900

    chore: profile not found page 에러 수정

--- src/app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import UserProfile from '@/components/pages/UserProfile';
-import React from 'react';
-
-const page = () => {
-  return <div>Profile</div>;
-  // return <UserProfile />;
-};
-
-export default page;
